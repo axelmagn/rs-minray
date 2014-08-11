8c206bdaa93b14f5f574b36fd0ad4a1caabbe4b7
Axel Magnuson <axelmagn@gmail.com>
2014-08-11 14:43:57 -0600

    more work to get this compiling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::rand::Rng;
 use std::rand;
 use std::iter::range_step;
 use std::iter;
-use std::num::pow;
+use std::num::{pow, Float};
 
 struct Vec3 {
     x: f64,
@@ -92,18 +92,95 @@ fn R() -> f64 {
 
 static G: &'static [i32] = &[247570,280596,280600,249748,18578,18577,231184,16,16];
 
+// (S)ample the world and return the pixel color for a ray passing by point o (Origin) and d
+// (Direction)
 fn S(o: Vec3, d: Vec3) -> Vec3 {
     // Search for an intersection ray Vs World
+    // m - status
+    // t - intersect distance
+    // n - 
     let (m, t, n) = T(o, d);
 
     if(m == 0) {
+        // No sphere was found and the ray goes upward.  Generate a sky color.
         return Vec3::new(0.7, 0.6, 1.0) * pow(1.0-d.z,4)
     }
-    Vec3::new(0.0,0.0,0.0)
+
+    // A sphere was maybe hit
+    
+    let mut h: Vec3 = o + d * t;                                  // h = intersection coordinate
+    let l = !(Vec3::new(9.0+R(),9.0+R(),16.0)+h*-1.0);  // direction to the light (with random 
+                                                        // delta for soft shadows
+    let r = d+n * (n % d * -2.0);                             // the half-vector
+    
+    // Calculated the lambertian factor
+    let b = l % n;
+
+    // Calculate the illumination factor.  Trace a ray from intersection to the light source to
+    // make sure it reaches.
+    let (i, t, n) = T(h, l);
+    if(b < 0.0 || i > 0) {
+        b = 0;
+    }
+
+    // calculate illumination factor
+    let p: f64 = pow(l % r * (b>0), 99);
+
+    if(m == 1) { 
+        // no sphere was hit and ray is going down.  Generate floor color
+        h = h * 0.2;
+        return (if(((h.x).ceil()+(h.y).ceil()) as u32 & 1) {
+                Vec3::new(3.0, 1.0, 1.0)
+            } else {
+                Vec3::new(3.0, 3.0, 3.0)
+            }) * (b * 0.2 + 0.1);
+    }
+
+    // m == 2
+    return Vec3::new(p,p,p) + S(h, r) * 0.5
+
 }
 
-fn T(o: Vec3, d: Vec3) -> (u32, f64, Vec3) {
-    (0, 0.0, Vec3::origin())
+// The intersection test of line [o,v]
+// Return 2 if a hit was found (and also return distance t and bouncing ray n)
+// Return 1 if no hit was found but ray goes downward
+// Return 0 if no hit was found but ray goes upward
+//
+fn T(o: Vec3, d: Vec3) -> (u8, f64, Vec3) {
+    let mut t: f64  = 1e9;
+    let mut i: u8   = 0;
+    let mut p: f64  = o.z / d.z;
+    let mut n: Vec3;
+    let mut m: u8;
+
+    if(0.01<p) {
+        t = p;
+        n = Vec3::new(0.0, 0.0, 1.0);
+        m = 1;
+    }
+
+    for k in range_step(18u, -1, -1) {
+        for j in range_step(8u, -1, -1) {
+            if(G[j] & 1 << k != 0) { // for this line j, is there a sphere at column i?
+                let p: Vec3 = o + Vec3::new(-k as f64, 0.0, (-j-4) as f64);
+                let b: f64 = p % d;
+                let c: f64 = p % p - 1;
+                let q: f64 = b * b - c;
+
+                if(q > 0) {
+                    let s: f64 = -b - q.sqrt();
+                    if(s < t && s > 0.01) {
+                        t = s;
+                        n = !(p + d * t);
+                        m = 2;
+                    }
+                }
+            }
+        }
+    }
+
+    return (m, t, n);
+
 }
 
 fn main() {
@@ -119,6 +196,9 @@ fn main() {
             // Reuse Vec3 to store RGB colors
             let mut p = Vec3::new(13.0, 13.0, 13.0); // default color is almost black
 
+            let xf = x as f64;
+            let yf = y as f64;
+
             // cast 64 rays per pixel
             for i in range_step(64i, 0i, -1i) {
 
@@ -127,12 +207,12 @@ fn main() {
                 // Set the camera focal point and cast the ray
                 // Accumulate the color returned in the p variable
                 p = S(
-                    Vec3::new(17.0,16.0,8.0)+t,         // Ray Origin
-                    !(t*-1.0+(a*(R()+(x as f64))+b*((y as f64)+R())+c)*16.0)  // Ray direction with random deltas
-                                                        // for stochastic sampling
+                    Vec3::new(17.0,16.0,8.0)+t,                 // Ray Origin
+                    !(t*-1.0+(a*(R()+xf)+b*(yf+R())+c)*16.0)    // Ray direction with random deltas 
+                                                                // for stochastic sampling
                 )*3.5+p;    // +p for color accumulation
             }
-            print!("{}{}{}", p.x as i32, p.y as i32, p.z as i32);
+            print!("{}{}{}", p.x as u8 as char, p.y as u8 as char, p.z as u8 as char);
         }
     }
 }
